a741c2ad368befa03e8a560c11038df49f136b81
Chris H <holtchr12@gmail.com>
2019-12-11 19:29:17 -0600

    finished everything through page title

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['Age of Ondra', 'Oceans 8', 'The Dawn Wall', 'The Prestige'];
 
    constructor() { }
 
